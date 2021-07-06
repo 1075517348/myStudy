f8b1ca8683df66f4e99a7e7ccf0fe490ddd8842c
梁文浩 <5215204+liang_wenhao@user.noreply.gitee.com>
2021-07-06 16:39:08 +0800

    我的初次提交测试

---
+++ src/com/itheima/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import java.util.Arrays;
+
+public class HelloWorld {
+    int i = 1;
+
+    public static void main(String[] args) {
+        Boolean b = new Boolean("ok");
+        System.out.println(b);
+        System.out.println(b.toString());
+    }
+}
---
+++ src/com/itheima/MyTest.java
@@ -0,0 +1,22 @@
+package com.itheima;
+
+/**
+ * MyTest Class Use to Test
+ * <p>this is my test<p/>
+ * {@link com.itheima.HelloWorld#theTest()}
+ * {@link #testMyself()}
+ * {@link com.itheima.HelloWorld}
+ */
+public class MyTest {
+    static int number = 1;
+    public MyTest() {
+        System.out.println("测试构造方法");
+    }
+
+    public void testMyself() {
+
+    }
+    static {
+        System.out.println("测试static方法");
+    }
+}
